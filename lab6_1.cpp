7a41e052195936460c8c4a5826c1f6089f8dcd4b
Inhibitors <Tanakrit64566@gmail.com>
2022-12-09 15:42:04 +0700

    lab6.2 fin

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int num1;
+    int even = 0;
+    int odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num1;
+    while(num1 != 0){
+        if(num1%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> num1;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
